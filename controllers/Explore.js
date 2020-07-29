ee48021cefa74b477a525d75c42251cefd752a91
hdriqi <riqi.asdf@gmail.com>
2020-07-29 20:56:40 +0700

    update search API

--- controllers/Explore.js
+++ controllers/Explore.js
@@ -25,11 +25,11 @@ class Explore {
     const data = await this.storage.db.collection('post').aggregate([{ $sample: { size: maxSample } }])
 
     const arr = await data.toArray()
-    const selected = [arr[rng]] 
+    const selected = [arr[rng]]
     const iter = selected.map(x => x)
     const result = []
     for await (const d of iter) {
-      if (embed &&  embed.length > 0) {
+      if (embed && embed.length > 0) {
         for (const e of embed) {
           d[e.col] = await this.storage.db.collection(e.targetCol).findOne({
             [e.targetKey]: d[e.key]
@@ -48,21 +48,45 @@ class Explore {
 
     const options = {
       includeScore: true,
-      sortFn: (a,b) => b.score - a.score,
+      sortFn: (a, b) => b.score - a.score,
       keys: ['id']
     }
-    
-    const fuse = new Fuse(combineList, options)
-    const result = fuse.search(q.id__re)
-    const itemList = result.slice(0, 10).map(res => {
-      return {
-        id: res.item.id,
-        type: res.item.imgAvatar ? 'user' : 'memento',
-        img: res.item.img || res.item.imgAvatar
-      }
-    })
 
-    return itemList
+    const fuse = new Fuse(combineList, options)
+    const result = fuse.search(q.id__re).slice(0, 10)
+    const final = []
+    for (const data of result) {
+      const postQuery = {
+        __limit: 3,
+        __sort: '-createdAt'
+      }
+      if (data.item.imgAvatar) {
+        postQuery.owner = data.item.id
+      }
+      else {
+        postQuery.mementoId = data.item.id
+      }
+      const postList = await this.storage.get('post', postQuery, [{
+        col: 'memento',
+        key: 'mementoId',
+        targetCol: 'memento',
+        targetKey: 'id'
+      }, {
+        col: 'user',
+        key: 'owner',
+        targetCol: 'user',
+        targetKey: 'id'
+      }])
+
+      final.push({
+        id: data.item.id,
+        type: data.item.imgAvatar ? 'user' : 'memento',
+        img: data.item.img || data.item.imgAvatar,
+        postList: postList
+      })
+    }
+
+    return final
   }
 }
 
